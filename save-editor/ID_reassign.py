a558e0141a513a244cde541612010ec234ae7840
Snowstar38 <snowstar38@gmail.com>
2023-09-29 23:05:34 -0400

    Update ID_reassign.py

--- save-editor/ID_reassign.py
+++ save-editor/ID_reassign.py
@@ -7,33 +7,38 @@ clan_name = sys.argv[1]
 # Now generate the file paths
 filename = f"../saves/{clan_name}/clan_cats.json"
 
-# Now use these paths in the rest of your script
+# Load the file
 with open(filename, "r") as file:
     cats = ujson.load(file)
 
 IDs = {"leader": 1, "deputy": 2, "medicine cat": 3}
-id_count = 4
+id_count = 1
 id_mates_apprentices = {"mate", "previous_mates", "current_apprentice", "former_apprentices", "former_mentor"}
-id_mentor = {"mentor"}
+
+normal_cats = []  # A list to store the remaining cats 
 
 # First handle the special positions
 for cat in cats:
     if cat['status'] in IDs and cat['dead'] == False:
-        cat['ID'] = str(IDs[cat['status']])
-        if cat['status'] == 'medicine cat':
-            IDs[cat['status']] += 1  # Increment ID for next medicine cat
+        cat['ID'] = str(id_count)
+        id_count += 1  # Increment ID for next cat
         # Clear out related cats
         for key in id_mates_apprentices:
             if key in cat:
                 cat[key] = []
         cat['mentor'] = None
     else:
-        cat['ID'] = str(id_count)
-        id_count += 1
-        # Clear out related cats
-        for key in id_mates_apprentices:
-            if key in cat:
-                cat[key] = []
+        # If not a special cat, add to normal_cats for later processing
+        normal_cats.append(cat)
+
+# Now handle the normal cats
+for cat in normal_cats:
+    cat['ID'] = str(id_count)
+    id_count += 1
+    # Clear out related cats
+    for key in id_mates_apprentices:
+        if key in cat:
+            cat[key] = []
 
 # Write the sorted JSON data back to the file
 with open(filename, "w") as file:
